f89d3da20a59c000214091f17ab17c5afc72fc0b
zainulmutaqin <zainulmutaqin15@gmail.com>
2019-05-24 13:05:32 +0700

    Updates

---
+++ precache-manifest.c23981be75bef38efa250765db5e9e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b03bf173cdc0dda69d7b9a70891fb4",
+    "url": "/Profile-Web/index.html"
+  },
+  {
+    "revision": "f121fc0d5b1cc640a087",
+    "url": "/Profile-Web/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/Profile-Web/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "f121fc0d5b1cc640a087",
+    "url": "/Profile-Web/static/js/main.50d258fa.chunk.js"
+  },
+  {
+    "revision": "99911b5e8aa9b28b942c",
+    "url": "/Profile-Web/static/js/runtime~main.495c37aa.js"
+  }
+]);
